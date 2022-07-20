eb56c97a08133cbf032b723bc6c8f8931dd0d287
Rachel Wong <109103462+rachelwong44@users.noreply.github.com>
2022-07-20 19:32:16 +0100

    Final touches

--- rock_paper_scissors/manual_rps.py
+++ rock_paper_scissors/manual_rps.py
@@ -36,15 +36,17 @@ class rock_paper_scissors:
 
 
     def get_user_choice(self):
-        print("Please input from keyboard R=Rock, P=Paper, S=Scissors")
         letter= str(input('\nInput User choice: ')).upper()
         out = ""
         if letter == "R":
             out = "Rock"
-        if letter =="P":
+        elif letter =="P":
             out = "Paper"
-        if letter == "S":
+        elif letter == "S":
             out = "Scissors"
+        else:
+            print("Please input either R, P, or S")
+            self.get_user_choice()
         #print(f"User choice: {out}")
         return out 
 
@@ -52,6 +54,7 @@ def play_game(rps_list):
     game = rock_paper_scissors(rps_list, num_lives=0)
     while game.num_lives<= 3 and game.pcnum_lives<= 3:
         a=game.get_computer_choice()
+        print("Please input from keyboard R=Rock, P=Paper, S=Scissors")
         b=game.get_user_choice()
         game.get_winner(a, b)
         print(f'\nTotal computer wins {game.pcnum_lives} and Total user wins {game.num_lives}')
