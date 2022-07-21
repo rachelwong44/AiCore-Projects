9938729f8c0365c274ac49d7fc186267080bc216
Rachel Wong <109103462+rachelwong44@users.noreply.github.com>
2022-07-21 12:58:59 +0100

    Game without user input

--- rock_paper_scissors/camera_rps.py
+++ rock_paper_scissors/camera_rps.py
@@ -1,57 +1,51 @@
 import cv2
-from keras.models import load_model
+print("Loading model - be patient...\n")
+print("\n...\n")
+#from keras.models import load_model
 import numpy as np
 import random
 import time 
 
-model = load_model('keras_model.h5')
-cap = cv2.VideoCapture(0)
-
 class rock_paper_scissors:
     def __init__(self, rps_list, num_lives, pcnum_lives):
-        self.model = load_model('keras_model.h5')
         self.num_lives=num_lives
         self.rps_list=rps_list
         self.rps_list=['Rock', 'Paper', 'Scissors']
         self.pcnum_lives=pcnum_lives
         self.start=time.time()
-        time_elapsed=[]
         self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        print("\nStill loading model...\n")
+        #self.model = load_model('keras_model.h5')
+        self.cap = cv2.VideoCapture(0)
 
     def countdown(self): 
         while True:
-                end=time.time()
-                time_elapsed=(round(end-self.start,1))
-                #print(time_elapsed)
-                if time_elapsed == 1:
-                    #print(f'One')
-                    print(f'{time_elapsed}s')
-                    break
+            end=time.time()
+            time_elapsed=(round(end-self.start,1))
+            if time_elapsed == 1:
+                print(f'{time_elapsed}s')
+                break
         while True:
+            end=time.time()
+            time_elapsed=(round(end-self.start,1))
+            if time_elapsed == 2:
                 end=time.time()
-                time_elapsed=(round(end-self.start,1))
-                if time_elapsed == 2:
-                    end_sec=time.time()
-                    print(f'{time_elapsed}s')
-                    break
+                print(f'{time_elapsed}s')
+                break
         while True:
-                end=time.time()
-                time_elapsed=(round(end-self.start,1))
-                if time_elapsed == 3:
-                    print(f'{time_elapsed}s')
-                    break 
-        return 
+            end=time.time()
+            time_elapsed=(round(end-self.start,1))
+            if time_elapsed == 3:
+                print(f'{time_elapsed}s')
+                break 
 
     def camera(self):
-        while True: 
-            ret, frame = cap.read()
-            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-            image_np = np.array(resized_frame)
-            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-            self.data[0] = normalized_image
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        ret, self.frame = cap.read()
+        resized_frame = cv2.resize(self.frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        self.data[0] = normalized_image
+        cv2.imshow('frame', self.frame)
 
     def get_prediction(self):         #return output of model with probabilities = confidence - pick highest probability
         predictions = self.model.predict(self.data)
@@ -88,11 +82,14 @@ class rock_paper_scissors:
 def play(rps_list):
     game = rock_paper_scissors(rps_list, num_lives=0, pcnum_lives=0)
     while game.num_lives<= 3 and game.pcnum_lives<= 3:
-        game.camera()
-        cap.release()
-        cv2.destroyAllWindows()
+        #game.camera()
+        #cap.release()
+        #cv2.destroyAllWindows()
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
+        print("Show your hand after a count to 3:")
+        game.countdown()
         a=game.get_computer_choice()
-        print("Please input from keyboard R=Rock, P=Paper, S=Scissors")
         b=game.get_prediction()
         game.get_winner(a, b)
         print(f'\nTotal computer wins {game.pcnum_lives} and Total user wins {game.num_lives}')
@@ -102,6 +99,8 @@ def play(rps_list):
         if game.num_lives == 3: 
             print('\nUser wins the game!')
             break
+        print('Press q to close the window')
+
 
 if __name__ == '__main__':
     rps_list = ['Rock','Paper','Scissors']
